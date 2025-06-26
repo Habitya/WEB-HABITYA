42a033a34c5f84828505682eeed0f512e5839a82
Habitya <habitya.living@gmail.com>
2025-06-26 16:33:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -185,3 +185,11 @@ function submitInvoice() {
   closeModal();
   alert('Factura subida correctamente');
 }
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.header');
+  if (window.scrollY > 20) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
